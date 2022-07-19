43b9eb43d6f6c107262cfa2a2bc0ecb6b512175e
queeny1945 <109617191+queeny1945@users.noreply.github.com>
2022-07-20 00:32:16 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
     session_start(); //inisialisasi session
     if(session_destroy()) {//menghapus session
-        header("Location: ../index.php"); //jika berhasil maka akan diredirect ke file index.php
+        header("Location: index.php"); //jika berhasil maka akan diredirect ke file index.php
     }
 ?>
